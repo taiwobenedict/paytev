02b61a5d23811230f879ef8c1b9dc4d25fbe3a01
Taiwo <ajayitaiwotunta@gmail.com>
2024-09-01 21:36:32 +0100

    updated

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from main.models import AppInfo , ActivationKeys
+from main.models import AppInfo
 
 admin.site.register(AppInfo)
-admin.site.register(ActivationKeys)
